9385b46980003b13d76f89686ab6d9437e0eddea
Avi <avibanerjee2000@gmail.com>
2024-04-16 20:26:14 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css'
 import { inter } from "@/app/ui/fonts";
 import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Acme Dashboard',
